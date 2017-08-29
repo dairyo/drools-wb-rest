f0ef89bd6a0be732b53747e44f505539a14ceee1
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-08-29 18:18:47 +0900

    Fix comments.

--- scripts/add-user.sh
+++ scripts/add-user.sh
@@ -2,19 +2,19 @@
 
 USER_NAME=test
 
-# create user
+# create user.
 curl -v -X POST -u "rest:rest123" \
   -H "Content-Type:application/json" -H "Accept: application/json" \
   "http://localhost:8080/drools-wb/rest/user" \
   -d "${USER_NAME}"
 
-# assign a role
+# assign a role.
 curl -v -X PUT -u "rest:rest123" \
   -H "Content-Type:application/json" -H "Accept: application/json" \
   "http://localhost:8080/drools-wb/rest/user/roles/${USER_NAME}" \
   -d '["analyst"]'
 
-# assign a role
+# assign password.
 curl -v -X PUT -u "rest:rest123" \
   -H "Content-Type:application/json" -H "Accept: application/json" \
   "http://localhost:8080/drools-wb/rest/user/password/${USER_NAME}" \
