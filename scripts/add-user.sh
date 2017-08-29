4b41fa12edc324c1e5a423fba18f2d45cb789bbe
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-08-29 18:17:49 +0900

    Add test script to add user.

---
+++ scripts/add-user.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+USER_NAME=test
+
+# create user
+curl -v -X POST -u "rest:rest123" \
+  -H "Content-Type:application/json" -H "Accept: application/json" \
+  "http://localhost:8080/drools-wb/rest/user" \
+  -d "${USER_NAME}"
+
+# assign a role
+curl -v -X PUT -u "rest:rest123" \
+  -H "Content-Type:application/json" -H "Accept: application/json" \
+  "http://localhost:8080/drools-wb/rest/user/roles/${USER_NAME}" \
+  -d '["analyst"]'
+
+# assign a role
+curl -v -X PUT -u "rest:rest123" \
+  -H "Content-Type:application/json" -H "Accept: application/json" \
+  "http://localhost:8080/drools-wb/rest/user/password/${USER_NAME}" \
+  -d 'password'
+
